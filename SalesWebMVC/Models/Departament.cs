59cf1455bd07449f03e1ee3ac8f27c08e5508857
Ruan Pablo <ruanpablocutrimramon@gmail.com>
2025-01-19 11:43:10 -0300

    Second migration

--- SalesWebMVC/Models/Departament.cs
+++ SalesWebMVC/Models/Departament.cs
@@ -1,8 +1,28 @@
-﻿namespace SalesWebMVC.Models
+﻿using System.Collections.Generic;
+namespace SalesWebMVC.Models
 {
     public class Departament
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+        public Departament()
+        {
+        }
+
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
+    
 }
